2efab4244e1753bd78796b12a666bfea6c540fd3
minyoung kim <55653869+min0k0311@users.noreply.github.com>
2021-07-08 01:34:33 +0900

    bottom nav bar

---
+++ app/src/main/java/com/example/a2021_wedge/MyPageFrag/MyPageFrag.java
@@ -0,0 +1,22 @@
+package com.example.a2021_wedge.MyPageFrag;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.a2021_wedge.R;
+
+public class MyPageFrag extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.frag_my_page, container, false);
+
+        return v;
+    }
+}
---
+++ app/src/main/java/com/example/a2021_wedge/StoreFrag/StoreFrag.java
@@ -0,0 +1,22 @@
+package com.example.a2021_wedge.StoreFrag;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.a2021_wedge.R;
+
+public class StoreFrag extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.frag_store, container, false);
+
+        return v;
+    }
+}
