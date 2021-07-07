72a804ea3292fe92c561793a9a1e1d4a3ede2531
minyoung kim <55653869+min0k0311@users.noreply.github.com>
2021-07-08 04:12:21 +0900

    mypage

--- app/src/main/java/com/example/a2021_wedge/Login.java
+++ app/src/main/java/com/example/a2021_wedge/Login.java
@@ -1,17 +1,13 @@
 package com.example.a2021_wedge;
 
-import android.annotation.SuppressLint;
-
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import com.example.a2021_wedge.bottomBar.MainActivity;
 
 
 public class Login extends AppCompatActivity {
