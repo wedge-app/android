eb8d91d98b5eb5d345d91f55d7c2646c435080d1
yuulee112 <yourim0112@naver.com>
2021-07-08 01:10:59 +0900

    main+login+join

--- app/src/main/java/com/example/a2021_wedge/Main.java
+++ app/src/main/java/com/example/a2021_wedge/Main.java
@@ -2,13 +2,35 @@ package com.example.a2021_wedge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Main extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView4);
+        imageView.setVisibility(View.INVISIBLE);
+
+        ImageView imageView2 = findViewById(R.id.imageView5);
+        imageView2.setVisibility(View.INVISIBLE);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            imageView2.setVisibility(View.VISIBLE);
+            imageView.setVisibility(View.VISIBLE);
+        }, 500);
+
+
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+        }, 2000);
     }
 }
