307f04d55c61b63cd5db89806fe2cffc4267dc67
yuulee112 <yourim0112@naver.com>
2021-07-08 21:46:22 +0900

    +search

---
+++ app/src/main/java/com/example/a2021_wedge/First/join.java
@@ -0,0 +1,43 @@
+package com.example.a2021_wedge;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+
+public class join extends AppCompatActivity {
+    ImageButton fin, back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+
+        //뒤로 가기 버튼
+        back = findViewById(R.id.imageButton2);
+        back.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+        });
+
+        //완료 버튼
+        fin = findViewById(R.id.imageButton3);
+        fin.setOnClickListener(v -> {
+            //나중에 데베 연결 후 수정하기
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+        });
+    }
+
+}
