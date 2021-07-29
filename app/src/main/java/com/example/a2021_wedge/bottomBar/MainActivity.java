4f126b1e493079b63e7f65277a22acf5069021ab
goeun208 <qop341@gmail.com>
2021-07-29 18:32:49 +0900

    yato

--- app/src/main/java/com/example/a2021_wedge/bottomBar/MainActivity.java
+++ app/src/main/java/com/example/a2021_wedge/bottomBar/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.a2021_wedge.bottomBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.example.a2021_wedge.HomeFrag.HomeFrag;
 import com.example.a2021_wedge.MyPageFrag.MyPageFrag;
@@ -10,6 +14,17 @@ import com.example.a2021_wedge.R;
 import com.example.a2021_wedge.SearchFrag.SearchFrag;
 import com.example.a2021_wedge.StoreFrag.StoreFrag;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity  {
 
@@ -45,4 +60,5 @@ public class MainActivity extends AppCompatActivity  {
         }
 
     }
+
 }
