90628249ee3e3e4a140898419af00c51b756b6ad
yuulee112 <yourim0112@naver.com>
2021-08-05 19:43:34 +0900

    storejoin

--- app/src/main/java/com/example/a2021_wedge/storejoin2.java
+++ app/src/main/java/com/example/a2021_wedge/storejoin2.java
@@ -19,6 +19,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.a2021_wedge.First.Login;
+import com.example.a2021_wedge.bottomBar.MainActivity;
 import com.example.a2021_wedge.databinding.ActivityStorejoin2Binding;
 
 import org.xmlpull.v1.XmlPullParser;
@@ -37,13 +38,26 @@ public class storejoin2 extends AppCompatActivity {
     private static final int SEARCH_ADDRESS_ACTIVITY = 10000;
     private EditText et_address;
 
-    ImageButton search;
+    ImageButton search, back, storesubmit;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_storejoin2);
+
+        //뒤로 가기 버튼
+        back = findViewById(R.id.storeback2);
+        back.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), Storejoin1.class);
+            startActivity(intent);
+        });
+
+        storesubmit = findViewById(R.id.storesubmit);
+        back.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        });
 
         et_address = (EditText) findViewById(R.id.et_address);
         search = findViewById(R.id.srh);
