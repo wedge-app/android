841bc5dce32e87a92979384842e955df39c3c872
yuulee112 <yourim0112@naver.com>
2021-08-11 21:56:51 +0900

    join

---
+++ app/src/main/java/com/example/a2021_wedge/retrofit/RegisterRequest.java
@@ -0,0 +1,32 @@
+package com.example.a2021_wedge.retrofit;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+
+    //서버 URL 설정(php 파일 연동)
+    final static private  String URL = "http://mywedge21.dothome.co.kr/signup.php";
+    private Map<String, String> map;
+
+
+    public RegisterRequest(String userID, String userPassword, String userName, String userTel, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+        map = new HashMap<>();
+        map.put("userID", userID);
+        map.put("userPassword", userPassword);
+        map.put("userName", userName);
+        map.put("userTel", userTel);
+    }
+
+
+    @Override
+    protected Map<String, String>getParams() throws AuthFailureError {
+        return map;
+    }
+}
