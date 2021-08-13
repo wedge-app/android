d97ee53ce57aa6ce71f10bdc5c42c416e78b496d
minyoung kim <55653869+min0k0311@users.noreply.github.com>
2021-08-13 22:27:06 +0900

    searchlist

---
+++ app/src/main/java/com/example/a2021_wedge/SearchFrag/OnLatelySearchListItemClickListener.java
@@ -0,0 +1,9 @@
+package com.example.a2021_wedge.SearchFrag;
+
+import android.view.View;
+
+import com.example.a2021_wedge.Sajang.WaitingListAdapter;
+
+public interface OnLatelySearchListItemClickListener {
+    public void onItemClick(LatelySearchListAdapter.ViewHolder holder, View view, int position);
+}
