dd5c7fc611e2d681a0593938b1e140659afa4a4a
yuulee112 <yourim0112@naver.com>
2021-08-19 15:30:15 +0900

    22

--- app/src/main/java/com/example/a2021_wedge/Rev/ReviewList.java
+++ app/src/main/java/com/example/a2021_wedge/Rev/ReviewList.java
@@ -88,8 +88,7 @@ public class ReviewList extends AppCompatActivity {
             }
         });
 
-        ImageButton b = findViewById(R.id.b);
-
+        ImageButton b = findViewById(R.id.back);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -251,17 +250,9 @@ public class ReviewList extends AppCompatActivity {
 
     }
 
-    private long time= 0;
     @Override
     public void onBackPressed() {
-        if (System.currentTimeMillis() - time >= 2000) {
-            time = System.currentTimeMillis();
-            Toast.makeText(getApplicationContext(), "뒤로 버튼을 한번 더 누르면 종료합니다.", Toast.LENGTH_SHORT).show();
-        } else if (System.currentTimeMillis() - time < 2000) {
-            finishAffinity();
-            System.runFinalization();
-            System.exit(0);
-        }
+        finish();
     }
 
 
